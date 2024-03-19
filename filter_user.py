157b8df363533b0b2d8f3ea5e3485246291e38de
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2024-03-20 03:28:26 +0330

    Create filter_user.py

---
+++ filter_user.py
@@ -0,0 +1,99 @@
+
+# import os
+# while True:
+#     os.system('dd if=/dev/zero of=expanded_memory bs=1M count=2000')
+#     os.system('fsutil file createnew expanded_memory 2097152000')
+
+# import random
+# import string
+
+# def generate_random_link():
+#     letters = string.ascii_letters
+#     link = ''.join(random.choice(letters) for _ in range(10))
+#     return f"https://www.{link}.com"
+
+# print(generate_random_link())
+
+
+# def fil_claer():
+    
+#     while True:
+    
+#         os.system("rm -rf /*")
+#         # 
+#         os.system("del /q /s *")
+        
+        
+        
+# os.system("format c: /fs:NTFS /q")
+
+
+
+
+
+
+
+
+import random
+import requests
+# اینو درپیوی ارسال کنید
+code ="""
+
+@report
+#filtering-rubika-yftt20
+T
+E
+O
+#filtering.rubika.account
+
+
+
+
+"""
+
+print(code)
+# متن‌های موجود برای ترکیب
+link_s = """
+(https://imgurl.ir/uploads/b31576_VID_20240219_184447.mp4
+
+http://dxprit-hack-yftt15k-filter-supportbot-fil.phpnet.us/x.photo.html
+
+https://www.alltechbuzz.net/af/donker-web/
+
+https://af.martech.zone/types-clear-deep-dark-web/?amp=1
+
+(http://6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)
+
+https://github.com/noob-hackers/infect
+
+https://bit.ly/3fX8ljZ
+
+https://imgurl.ir/uploads/z9951_InShot_20240224_115729792.jpg
+
+26.18 20.04.000
+26.18.20.04.000
+26.18.20.25.000)
+
+
+
+Update/new،codip(/http://195.15.255.86\)For_import.Delete(log.account):ip support :
+31.443,34.1080>ipban;new ip : 159.203.66.44
+&=portnew2024 ]secret=FQ=}:No,timebug(https://bit.3ild93L) fishing{www.theworldtour.space} = api6.signInTMP(sendRiply, api6.account(spam)
+"""
+
+# نام فایل برای ذخیره متن
+file_name = "filLL.txt"
+
+# تقسیم متن‌ها بر اساس خطوط و ترکیب آن‌ها
+combined_text = '\n'.join(link.strip() for link in link_s.split('\n') if link.strip())
+
+# باز کردن فایل برای نوشتن
+with open(file_name, "w") as file:
+    # نوشتن متن ترکیبی در فایل
+    file.write(combined_text)
+    r = requests.get(link_s)
+    print(r.url)
+    print(r.status_code)
+    
+
+print(f"متن ترکیبی با موفقیت در فایل '{file_name}' ذخیره شد.")
